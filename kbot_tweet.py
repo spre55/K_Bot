f48bcc3a941cacb6b3c610a3c5443f67b2a4e119
sprechchor <tatsuya.spre@gmail.com>
2015-06-15 16:47:33 +0900

    Overlap Delete(in the midle)

--- kbot_tweet.py
+++ kbot_tweet.py
@@ -7,9 +7,26 @@ import key,twt_cntnts,random
 # ツイート投稿用のURL
 url = "https://api.twitter.com/1.1/statuses/update.json"
 # twt_cntntsからツイートを取得する為のキー生成
-rdm_key = random.randint(1,10)
+def CreateRDMKey():
+	return random.randint(1,10)
+
+rdm_key = CreateRDMKey
 # ツイート本文
-params = {"status": twt_cntnts.twt(rdm_key)}
+params = {"status": twt_cntnts.twt(CreateRDMKey)}
+
+# ツイートの重複をチェック
+def twt_OverlapCheck(twt):
+	if twt == twt_list[-1]:
+		rdm_key = CreateRDMKey
+	else:
+		twt_list = append(rdm_key)
+
+	#twt_list の最大値を3に維持
+	if max(twt_list) == 1:
+		twt_list = []
+
+	return twt
+
 
 # OAuth認証で POST method で投稿
 twitter = OAuth1Session(key.CK, key.CS, key.AT, key.AS)
